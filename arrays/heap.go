c4d9b5d0da97823913e75b00b3540f8ccfc0d8db
SpandanBG <spandanbg@gmail.com>
2025-08-03 21:40:07 +0530

    add optimized solve for max heap sliding window

--- arrays/heap.go
+++ arrays/heap.go
@@ -1,63 +1,5 @@
 package arrays
 
-import (
-	"container/heap"
-)
-
-type Item struct {
-	value int
-	index int
-}
-
-type PQue struct {
-	maxHeap   []*Item
-	entryHeap []*Item
-}
-
-func (p PQue) Len() int {
-	return len(p.maxHeap)
-}
-
-func (p PQue) Less(i, j int) bool {
-	return p.maxHeap[i].value > p.maxHeap[j].value
-}
-
-func (p PQue) Swap(i, j int) {
-	p.maxHeap[i], p.maxHeap[j] = p.maxHeap[j], p.maxHeap[i]
-	p.maxHeap[i].index = i
-	p.maxHeap[j].index = j
-}
-
-func (p *PQue) Push(x any) {
-	switch y := x.(type) {
-	case int:
-		n := len(p.maxHeap)
-		item := &Item{
-			value: y,
-			index: n,
-		}
-
-		if len(p.maxHeap) == 0 {
-			p.maxHeap = []*Item{item}
-			p.entryHeap = []*Item{item}
-		} else {
-			p.maxHeap = append(p.maxHeap, item)
-			p.entryHeap = append(p.entryHeap, item)
-		}
-	default:
-		return
-	}
-}
-
-func (p *PQue) Pop() any {
-	return nil
-}
-
-func (p *PQue) update(i *Item, value int) {
-	i.value = value
-	heap.Fix(p, i.index)
-}
-
 // You are given an array of integers nums, there is a sliding window of size k
 // which is moving from the very left of the array to the very right. You can only
 // see the k numbers in the window. Each time the sliding window moves right by one
@@ -85,27 +27,63 @@ func (p *PQue) update(i *Item, value int) {
 // Input: nums = [1], k = 1
 // Output: [1]
 func MaxHeap_SlidingWindowMax(nums []int, k int) []int {
-	ans := make([]int, 0)
+	/*Note
+	We are going to create a priority queue where we will only keep the index of
+	the largest number at any given point of the sliding window.
 
-	/* Note: */
+	At each iteration, we will first pop out (from the end) any numbers on the
+	queue that are smaller than the one we are going to insert iteratively.
 
-	p := PQue{
-		maxHeap:   make([]*Item, 0),
-		entryHeap: make([]*Item, 0),
+	Then we are going to push in the new number -> and calculate the start index
+	of the sliding window using (i - k + 1), where:
+		- i: the current index that has been entered
+		- k: the size of the window
+
+		The formula (i - k + 1) says:
+			- For the index `i`
+			- We will go back `k` steps
+			- And jump forward `+1` step to account for the newly added item
+
+	After finding the starting index of the window, we will check from the start
+	of the queue if any index index exists that is outside of the window and
+	remove them.
+
+	After our starting index of the window has reached 0, we will then start adding
+	the highest value (which will be at the beginning of the queue) to our answer
+	list.
+	*/
+
+	ans := make([]int, len(nums)-k+1)
+	i_ans, w_si := 0, 0
+
+	queue := make([]int, 0)
+
+	for i := 0; i < len(nums); i += 1 {
+		// First we remove all the elements in the queue which are smaller than the
+		// next item
+		for len(queue) > 0 && nums[queue[len(queue)-1]] <= nums[i] {
+			queue = queue[:len(queue)-1]
+		}
+
+		// Then we add the new index
+		queue = append(queue, i)
+
+		// Then we find out where the window's starting index is
+		w_si = i - k + 1
+
+		// Then remove all items from the beginning that has moved away from the
+		// start index of the window
+		for queue[0] < w_si {
+			queue = queue[1:]
+		}
+
+		// And finally after the entire window has entered the nums array -> we
+		// start collecting the the max values into our answers list
+		if w_si >= 0 {
+			ans[i_ans] = nums[queue[0]]
+			i_ans += 1
+		}
 	}
-
-	heap.Init(&p)
-
-	for _, value := range nums[:k] {
-		heap.Push(&p, value)
-	}
-
-	for i, value := range nums[k:] {
-		ans = append(ans, p.maxHeap[0].value)
-		next := p.entryHeap[i%k]
-		p.update(next, value)
-	}
-	ans = append(ans, p.maxHeap[0].value)
 
 	return ans
 }
